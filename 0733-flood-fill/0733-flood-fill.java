0f9416464ba3e27fbd50fa3a4244cd4add26780e
abhishekharshit928 <abhishekharshit928@gmail.com>
2026-09-26 11:04:53 +0530

    Time: 0 ms (100%), Space: 46.8 MB (43.03%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,29 +1,26 @@
 class Solution {
-    private void traversal(int i , int j , boolean[][] vis , int[][] grid , int color , int originalColor ){
-        vis[i][j] = true;
-        grid[i][j] = color;
-
-        if(i >= 0 && i-1 >= 0 && vis[i-1][j] == false && grid[i-1][j] == originalColor ) { // up
-            traversal(i-1 , j , vis , grid , color,originalColor);
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) {
+            dfs(image, sr, sc, color, newColor);
         }
-        if(i < grid.length  && i+1 < grid.length && vis[i+1][j] == false && grid[i+1][j] == originalColor) { // down
-            traversal(i+1 , j , vis , grid , color,originalColor);
-        }
-        if(j > 0 && j-1 >= 0 && vis[i][j-1] == false && grid[i][j-1] == originalColor) { // left
-            traversal(i , j-1 , vis , grid , color,originalColor);
-        }
-        if( j< grid[0].length && j+1 < grid[0].length && vis[i][j+1] == false && grid[i][j+1] == originalColor) { // right
-            traversal(i , j+1 , vis , grid , color,originalColor);
-        }
-
-    }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
-        boolean[][] vis = new boolean[m][n];
-        int originalColor = image[sr][sc];
-        if (originalColor == color) return image;
-        traversal(sr , sc , vis , image , color , originalColor);
         return image;
+    }
+    public void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r >= 1) {
+                dfs(image, r - 1, c, color, newColor);
+            }
+            if (c >= 1) {
+                dfs(image, r, c - 1, color, newColor);
+            }
+            if (r + 1 < image.length) {
+                dfs(image, r + 1, c, color, newColor);
+            }
+            if (c + 1 < image[0].length) {
+                dfs(image, r, c + 1, color, newColor);
+            }
+        }
     }
 }
